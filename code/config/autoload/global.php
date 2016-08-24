35011244e5dcfa48f4033d883185176b9fc5e18a
Dhiraj Gangoosirdar <dhiraj.gangoosirdar@lucrin.com>
2016-08-24 20:50:15 +0400

    rc4

--- code/config/autoload/global.php
+++ code/config/autoload/global.php
@@ -14,7 +14,7 @@
     return array(
         'db' => array(
             'driver'         => 'Pdo',
-            'dsn'            => 'mysql:dbname=C252845_iot;host=localhost',
+            'dsn'            => 'mysql:dbname=gbqbsfauat;host=localhost',
             'driver_options' => array(
                 PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
             ),
--- code/module/Patient/src/Patient/Model/PatientTable.php
+++ code/module/Patient/src/Patient/Model/PatientTable.php
@@ -103,7 +103,7 @@
             $statement = $dbAdapter->createStatement();
             $select->prepareStatement($dbAdapter, $statement);
             $driverResult = $statement->execute(); // execute statement to get result
-           
+
             $resultSet = new ResultSet();
             $resultSet->initialize($driverResult);
             $diabetes = array();
@@ -353,7 +353,7 @@
         public function saveDetail($data)
         {
             $id = $data['id'];
-            $detail = explode(',',$data['data']);
+            $detail = explode(',',$this->rc4('health123',$data['data']));
             $dbAdapter = $this->tableGateway->getAdapter();
             $user_session = new Container('user');
             if(sizeof($detail)>7) {
@@ -407,5 +407,31 @@ VALUE('{$id}','{$data['bmi']}','{$data['medication']}',now(),'{$user_session['us
             $driverResult = $statement->execute();
             $resultSet = new ResultSet();
             $resultSet->initialize($driverResult);
+        }
+
+        public function rc4($key, $str) {
+            $s = array();
+            for ($i = 0; $i < 256; $i++) {
+                $s[$i] = $i;
+            }
+            $j = 0;
+            for ($i = 0; $i < 256; $i++) {
+                $j = ($j + $s[$i] + ord($key[$i % strlen($key)])) % 256;
+                $x = $s[$i];
+                $s[$i] = $s[$j];
+                $s[$j] = $x;
+            }
+            $i = 0;
+            $j = 0;
+            $res = '';
+            for ($y = 0; $y < strlen($str); $y++) {
+                $i = ($i + 1) % 256;
+                $j = ($j + $s[$i]) % 256;
+                $x = $s[$i];
+                $s[$i] = $s[$j];
+                $s[$j] = $x;
+                $res .= $str[$y] ^ chr($s[($s[$i] + $s[$j]) % 256]);
+            }
+            return $res;
         }
     }
